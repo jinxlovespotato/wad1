93e30b22d5a7aa56237ac79b293f10e660cd06bc
Jenny Gallor <jennygallor11@gmail.com>
2026-07-30 23:36:35 +0800

    add buttons

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 let score = 0;
+
+const incrementBtn = document.querySelector('#increment');
+const decrementBtn = document.querySelector('#decrement');
+const resetBtn = document.querySelector('#reset');
+const display = document.querySelector('#display');
+
+incrementBtn.addEventListener('click', () => {
+    score++;
+    display.textContent = `Score: ${score}`;
+});
+
+
+decrementBtn.addEventListener('click', () => {
+    score--;
+    display.textContent = `Score: ${score}`;
+});
+
+resetBtn.addEventListener('click', () => {
+    score = 0;
+     display.textContent = `Score: ${score}`;
+})
